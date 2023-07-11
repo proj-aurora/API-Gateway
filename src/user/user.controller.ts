0bb13bf736d93b6526a105bea86ab7fa267781ec
HYUN-8265 <pumdie77@gmail.com>
2023-07-11 19:36:06 +0900

    add(user): add user service, upload user profile & get user profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Get, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Res,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 import 'dotenv/config';
 
 import { AuthGuard } from '../utils/auth.guard';
 import { Request } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
 
 @Controller('user')
 export class UserController {
@@ -57,5 +74,56 @@ export class UserController {
     return await this.user
       .send({ check: 'update' }, { userId, ...data })
       .toPromise();
+  }
+
+  @Post('upload')
+  @UseGuards(AuthGuard)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          const dir = path.resolve(__dirname, '../../profile');
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+          }
+          cb(null, dir);
+        },
+        filename: (req, file, cb) => {
+          const extension: string = path.parse(file.originalname).ext;
+          if (
+            extension !== '.png' &&
+            extension !== '.jpg' &&
+            extension !== '.jpeg'
+          ) {
+            cb(
+              new BadRequestException('Only png, jpg, jpeg files are allowed!'),
+              null,
+            );
+          } else {
+            cb(null, `${uuidv4()}${extension}`);
+          }
+        },
+      }),
+    }),
+  )
+  async uploadFile(@Request() req, @UploadedFile() file) {
+    const userId = req.user.sub;
+    const fileName = file.filename;
+    return await this.user
+      .send({ check: 'upload' }, { userId, fileName })
+      .toPromise();
+  }
+
+  @Get('image')
+  @UseGuards(AuthGuard)
+  async getImage(@Request() req, @Res() res: Response) {
+    // const imagePath = path.resolve(__dirname, data.path);
+    const userId = req.user.sub;
+    const fileName = await this.user
+      .send({ check: 'image' }, { userId })
+      .toPromise();
+
+    const filePath = path.resolve(__dirname, `../../profile/${fileName}`);
+    return res.sendFile(filePath);
   }
 }
