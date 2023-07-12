4dacca2f51e584c24f7f92d666902a5be4233371
HYUN-8265 <pumdie77@gmail.com>
2023-07-12 18:40:04 +0900

    add(agent): add agent service, get agent list, create agent

--- src/agent/agent.controller.ts
+++ src/agent/agent.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from '../utils/auth.guard';
 
 @Controller('agent')
 export class AgentController {
@@ -18,5 +19,23 @@ export class AgentController {
   @Post('syslog/recent')
   async syslog_recent(@Body() data: object) {
     return await this.agent.send({ check: 'syslog' }, data).toPromise();
+  }
+
+  @Post('agent/list')
+  @UseGuards(AuthGuard)
+  async agentList(@Request() req, @Body() data: object) {
+    const userId = req.user.sub;
+    return await this.agent
+      .send({ check: 'agentList' }, { ...data, userId })
+      .toPromise();
+  }
+
+  @Post('agent/create')
+  @UseGuards(AuthGuard)
+  async agentCreate(@Request() req, @Body() data: object) {
+    const userId = req.user.sub;
+    return await this.agent
+      .send({ check: 'agentCreate' }, { ...data, userId })
+      .toPromise();
   }
 }
